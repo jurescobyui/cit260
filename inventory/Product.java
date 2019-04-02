8c1945a59b92b4d1f8fc9d270affb438a07d7496
jurescobyui <esc18011@byui.edu>
2019-04-02 15:07:27 +0800

    added product class

---
+++ inventory/Product.java
@@ -0,0 +1,31 @@
+/*--------------------------------
+ *         Class Product         |
+ * ------------------------------|
+ * - id :  Int                   |
+ * - name : String               |
+ * - price : double              |
+ * ------------------------------|
+ * + setId() : void              |
+ * + setName()                   |
+ * + setPrice()                  |
+ * + getID()                     |
+ * + getName()                   |
+ * + getPrice()                  |
+ * *-----------------------------*/
+public class Product {
+	private int id;
+	private String name;
+	private double price;
+	
+	//no-arg constructor
+	public Product() {
+		
+		
+	}
+	
+	public Product(int id, String name, double price) {
+		
+	}
+	
+
+}// end class
